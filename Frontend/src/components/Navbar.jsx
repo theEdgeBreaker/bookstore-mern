55ef32b1877e41e174b347afc98f53e7af2eb8a3
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-25 13:51:38 +0530

    chore(navbar): reduce height & width of sun-moon icon

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -90,7 +90,7 @@ const Navbar = () => {
 
               {/* sun icon */}
               <svg
-                className="swap-off h-8 w-8 fill-current"
+                className="swap-off h-7 w-7 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
@@ -99,7 +99,7 @@ const Navbar = () => {
 
               {/* moon icon */}
               <svg
-                className="swap-on h-8 w-8 fill-current"
+                className="swap-on h-7 w-7 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
               >
