c8a96dc8b145971dfecc83730e2abf6faccb55bf
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-26 12:46:06 +0530

    fix: add prop validation and styling to Cards, integrate into FreeBook

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,5 +1,41 @@
-const Cards = () => {
-  return <div>Cards</div>;
+import PropTypes from "prop-types";
+
+const Cards = ({ item }) => {
+  console.log({ item });
+  return (
+    <>
+      <div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img src={item.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className="badge badge-outline hover:bg-pink-500 hover:text-white duration-200">
+                Buy Now
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+Cards.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    category: PropTypes.string,
+    price: PropTypes.number,
+  }).isRequired,
 };
 
 export default Cards;
--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -2,6 +2,7 @@ import list from "../data/bookList.json";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Cards from "./Cards";
 
 const FreeBook = () => {
   const filterData = list.filter((data) => data.category === "Free");
@@ -55,30 +56,9 @@ const FreeBook = () => {
 
         <div>
           <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-            <div>
-              <h3>7</h3>
-            </div>
-            <div>
-              <h3>8</h3>
-            </div>
+            {filterData.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
           </Slider>
         </div>
       </div>
