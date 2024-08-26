b16ab41cbcaef9880fa10daf4c6d09446ecfb26e
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-27 05:19:11 +0530

    chore: add React Router and update Navbar links

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import Course from "./components/Course";
 import Home from "./home/Home";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Home />
-      <Course />
+      {/* <Home />
+      <Course /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Course />} />
+      </Routes>
     </>
   );
 }
