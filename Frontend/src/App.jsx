a00adf95f3edc1f847b1b8dba498bd14c29fcd8e
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-26 12:08:12 +0530

    Integrate FreeBook component into App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import FreeBook from "./components/FreeBook";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <FreeBook />
       <Footer />
     </>
   );
