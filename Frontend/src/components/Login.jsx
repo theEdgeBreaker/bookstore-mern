57bbff1bc9ccb79a65352bbd1eb09d8b940085e0
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-30 23:37:33 +0530

    feat: Add login and signup pages with form validation using react-hook-form

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,47 +1,89 @@
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log("Errors:", errors);
+
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
+        <div className="modal-box border-[2px] px-12 py-10">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <Link
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-3 top-3"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
               ✕
-            </button>
+            </Link>
+            <h3 className="font-bold text-lg">Login</h3>
+
+            {/* Email */}
+            <div className="mt-4 space-y-2">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
+                autoComplete="username"
+              />
+              <br />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
+
+            {/* password */}
+            <div className="mt-4 space-y-2">
+              <span>Password</span>
+              <br />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
+                autoComplete="current-password"
+              />
+              <br />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
+
+            {/* Button */}
+            <div className="flex justify-around mt-4">
+              <button
+                type="submit"
+                className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200"
+              >
+                Login
+              </button>
+              <p>
+                Not registered?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Signup
+                </Link>
+              </p>
+            </div>
           </form>
-          <h3 className="font-bold text-lg">Login</h3>
-          {/* Email */}
-          <div className="mt-4 space-y-2">
-            <span>Email</span>
-            <br />
-            <input
-              type="email"
-              placeholder=" Enter your Email"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/* password */}
-          <div className="mt-4 space-y-2">
-            <span>Password</span>
-            <br />
-            <input
-              type="text"
-              placeholder=" Enter your Password"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          {/* Button */}
-          <div className="flex justify-around mt-4">
-            <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-              Login
-            </button>
-            <p>
-              Not registered?{" "}
-              <span className="underline text-blue-500 cursor-pointer">
-                Signup
-              </span>
-            </p>
-          </div>
         </div>
       </dialog>
     </div>
