36065becada9345f87312602612761395b6b787c
theEdgeBreaker <thesanjana2002@gmail.com>
2024-10-17 13:12:58 +0530

    feat(context):Implemented AuthContext and updated App & main for user authentication flow

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import Home from "./home/Home";
 import Courses from "./courses/Courses";
 import Contacts from "./contacts/Contacts";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
-
 import { Toaster } from "react-hot-toast";
+import { useAuth } from "../context/AuthProvider";
 
 export default function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
+
   return (
     <>
       <div className="  dark:bg-slate-900 dark:text-white light:bg-red-300">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/course" element={<Courses />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
           <Route path="/contact" element={<Contacts />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
